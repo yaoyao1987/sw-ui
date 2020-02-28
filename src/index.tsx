228788785ec39b8fae4f86f06dd5fbb960415ae6
yao <yao@yaos-MacBook-Pro.local>
2020-02-28 11:04:17 +0800

    修改组件

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export { default as Button } from './components/Button'
+export { default as Tag } from './components/Tag'
+export { default as Empty } from './components/Empty'
+export { default as Alert } from './components/Alert'
