70c151b60f7e48a56b3d7faec83120eee5c525c5
yao <yao@yaos-MacBook-Pro.local>
2020-02-28 11:21:41 +0800

    修改配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,9 +1,15 @@
 import { IBundleOptions } from 'father';
 
 const options: IBundleOptions = {
-  cjs: 'rollup',
-  esm: 'rollup',
+  cjs: 'babel',
+  esm: 'babel',
   doc: { typescript: true },
+  extractCSS: true,
+  lessInBabelMode: true,
+  runtimeHelpers: true,
+  autoprefixer: {
+    browsers: ['ie>9', 'Safari >= 6'],
+  }
 };
 
 export default options;
