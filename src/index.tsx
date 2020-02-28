3a14a392f69208bde1df2ffb3f782a2ca89f494d
yao <yao@yaos-MacBook-Pro.local>
2020-02-28 10:43:06 +0800

    修改ui

--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './components/Button'
-export { default as Tag } from './components/Tag'
-export { default as Empty } from './components/Empty'
-export { default as Alert } from './components/Alert'
